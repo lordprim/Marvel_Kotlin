30de4d8dbb8949a44522f2a76dc2c5a8dc81df93
Vitor Prim <59850339+lordprim@users.noreply.github.com>
2022-07-29 08:48:08 -0300

    Update Constants.kt

--- Marvel_API/app/src/main/java/com/example/marvel_api/Api/model/Constants.kt
+++ Marvel_API/app/src/main/java/com/example/marvel_api/Api/model/Constants.kt
@@ -2,5 +2,5 @@ package com.example.marvel_api.Api.model
 
 
 // Tokens de acesso a API Marvel
-const val API_KEY = "90d0eba204fe02a0b3ca4955c0100050"
-const val PRIVATE_KEY = "493a2360dee30fb25b19a6a8d4bb85663e4a73b5"
+const val API_KEY = ""
+const val PRIVATE_KEY = ""
